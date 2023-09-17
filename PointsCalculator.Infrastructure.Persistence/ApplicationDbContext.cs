462c5fb854f6bab990a5e3bfbaff750fa4c754b6
mahdignb <mahdignb@hotmail.com>
2023-09-17 20:32:50 +0330

    Refactor : Rename application layers and make it shorter

---
+++ PointsCalculator.Infrastructure.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<AgeScore> AgeScores { get; set; }
+
+    }
+}
