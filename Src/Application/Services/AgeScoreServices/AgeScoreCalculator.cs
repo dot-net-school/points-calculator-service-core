413be8b0289828d78cb58ede0cb274659e9c26d8
Mohadese Entezari <mohadese.entezary@gmail.com>
2023-09-22 12:06:33 +0330

    Add AgeScoreCalculatorService

---
+++ Src/Application/Services/AgeScoreServices/AgeScoreCalculator.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities.AgeScoreEntity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Services.AgeScoreServices;
+
+public class AgeScoreCalculator
+{
+    private readonly IApplicationDbContext _dbContext;
+    public AgeScoreCalculator(IApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public async Task<int> GetAgeScore(int age)
+    {
+        if (age > 45 || age < 18)
+            return 0;
+        AgeScore? ageScore = await _dbContext.AgeScores.FirstOrDefaultAsync(x => x.FromAge >= age && x.ToAge >= age);
+        return ageScore is null ? 0 : ageScore.Score;
+    }
+}
