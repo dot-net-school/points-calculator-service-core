674e65c9f61b2a55ba1df21278420f4cbf8c97d4
Mohadese Entezari <mohadese.entezary@gmail.com>
2023-09-22 17:09:44 +0330

    rename input of CalculateAgeScore

--- Src/Application/Services/AgeScoreCalculatorService.cs
+++ Src/Application/Services/AgeScoreCalculatorService.cs
@@ -11,13 +11,13 @@ public class AgeScoreCalculatorService : IScoreCalculatorService<int, int>
         _dbContext = dbContext;
     }
 
-    public async Task<int> CalculateScore(int input)
+    public async Task<int> CalculateScore(int age)
     {
-        if (input > 45 || input < 18)
+        if (age > 45 || age < 18)
             return 0;
 
         var ageScore = await _dbContext.AgeScores
-            .Where(x => x.FromAge >= input && x.ToAge >= input)
+            .Where(x => x.FromAge >= age && x.ToAge >= age)
             .Select(s => s.Score)
             .FirstOrDefaultAsync();
 
