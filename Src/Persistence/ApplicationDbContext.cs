a67f79594c76739a4cfc6391a06938cfefd5c493
“Mehran-Kordavani” <w.laborer@gmail.com>
2023-10-22 11:15:16 +0330

    PCS-13 <Refactor: Add Unit Of Work and Edits for ApplicationDbContext.cs>

--- Src/Persistence/ApplicationDbContext.cs
+++ Src/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Entities;
+using System.Reflection;
+using Domain.Entities.LanguageScore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
@@ -8,9 +10,16 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
-
+        
     }
-   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("base");
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
     public DbSet<AgeScore> AgeScores { get; set; }
     public DbSet<JobExperienceScore> JobExperienceScores { get; set; }
+    public DbSet<LanguageCertification> LanguageCertifications { get; set; }
+    public DbSet<LanguageCertificationScore> LanguageScores { get; set; }
 }
---
+++ Src/Persistence/UnitOfWork/ApplicationUnitOfWork_Partial.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Common;
+using Domain.Entities;
+using Domain.Entities.LanguageScore;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+
+namespace Persistence.UnitOfWork;
+
+public partial class ApplicationUnitOfWork
+{
+    public DbSet<AgeScore> AgeScores { get; set; }
+    public DbSet<JobExperienceScore> JobExperienceScores { get; set; }
+    public DbSet<LanguageCertification> LanguageCertifications { get; set; }
+    public DbSet<LanguageCertificationScore> LanguageScores { get; set; }
+}
