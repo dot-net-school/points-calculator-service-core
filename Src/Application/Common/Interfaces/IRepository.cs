ac621b865064274590c372f20212a0e9fe19fa1e
“Mehran-Kordavani” <w.laborer@gmail.com>
2023-10-22 09:14:39 +0330

    PCS-13 <Refactor: OLD Generic Repository Interface>

--- Src/Application/Common/Interfaces/IRepository.cs
+++ Src/Application/Common/Interfaces/IRepository.cs
@@ -1,16 +1,19 @@
 ﻿using System.Linq.Expressions;
+using Domain.Common;
 
 namespace Application.Common.Interfaces;
 
-public interface IRepository<TEntity> where TEntity : class
+public interface IRepository<TEntity> where TEntity : BaseEntity
 {
     void Update(TEntity entity);
     void Delete(TEntity entity);
     Task AddAsync(TEntity entity, CancellationToken cancellationToken);
     IQueryable<TEntity> GetAll();
 
+    IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
     Task<TEntity> FindByIdAsync(object id, CancellationToken cancellationToken);
     Task SaveChangesAsync(CancellationToken cancellationToken);
     Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
-    IQueryable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    Task<TEntity> FirstOrDefaultAsNoTrackingAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
 }
