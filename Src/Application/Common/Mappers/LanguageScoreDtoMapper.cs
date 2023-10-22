89e90ba010209e3b443e7b1ed868737dfaf14679
“Mehran-Kordavani” <w.laborer@gmail.com>
2023-10-22 08:57:59 +0330

    PCS-13 <Refactor: Add Custom Validators And Dto For Queries>

---
+++ Src/Application/Common/Mappers/LanguageScoreDtoMapper.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs;
+using Domain.Entities.LanguageScore;
+
+namespace Application.Common.Mappers;
+
+public sealed class LanguageScoreDtoMapper
+{
+    public LanguageScoreDto Map(LanguageCertificationScore languageCertificationScore)
+    {
+        return new LanguageScoreDto(
+            languageCertificationScore.Id,
+            languageCertificationScore.Score.Value, // Map the Value property of Score to the Score property of the DTO
+            languageCertificationScore.Mark,
+            languageCertificationScore.IsActive
+        );
+    }
+
+    public List<LanguageScoreDto> MapList(List<LanguageCertificationScore> languageCertificationScores)
+    {
+        return languageCertificationScores.Select(Map).ToList();
+    }
+}
---
+++ Src/Application/Common/Validation/GuidValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Validation;
+
+public static class GuidValidator
+{
+    public static bool BeAValidGuid(string? id)
+    {
+        return Guid.TryParse(id,out _);
+    }
+}
