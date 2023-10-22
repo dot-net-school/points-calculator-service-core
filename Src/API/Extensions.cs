f74adcb0c7d00a1ff723bf6e7b5cd007b88c429d
“Mehran-Kordavani” <w.laborer@gmail.com>
2023-10-22 08:47:45 +0330

    PCS-13 <Refactor: Customize built-in Errors with action filter>

--- Src/API/Extensions.cs
+++ Src/API/Extensions.cs
@@ -1,9 +1,20 @@
-﻿namespace API;
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
 
 public static class Extensions
 {
     public static IServiceCollection RegisterPresentationServices(this IServiceCollection services)
     {
+        services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+        // this is for apply action filter of fluent validation or other library for validation rule
+        services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true;
+        });
+       
         return services;
     }
 }
---
+++ Src/API/Filters/ValidateModelStateAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shared;
+
+namespace API.Filters;
+
+public class ValidateModelStateAttribute :ActionFilterAttribute
+{
+    //TODO ask me why we choose this name fo it?
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var errors = context.ModelState.Values.Where(v => v.Errors.Count > 0)
+                .SelectMany(v => v.Errors)
+                .Select(v => v.ErrorMessage)
+                .ToList();
+
+            var responseObj = OperationResult<int>
+                .Failed(string.Join(", ", errors), HttpStatusCode.BadRequest);
+
+            context.Result = new JsonResult(responseObj)
+            {
+                StatusCode = 400
+            };
+        }
+    }
+}
