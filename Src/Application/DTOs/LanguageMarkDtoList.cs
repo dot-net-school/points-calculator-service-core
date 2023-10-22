9a8af899d7687b0044a6607f2be7feb9acd15f93
“Mehran-Kordavani” <w.laborer@gmail.com>
2023-10-22 08:55:50 +0330

    PCS-13 <Feat: Add Language Score Calculator Service With Its DTOs>

---
+++ Src/Application/DTOs/LanguageMarkDtoList.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.DTOs;
+
+public class LanguageMarkDtoList
+{
+    public List<LanguageMarkDto>? LanguageScoreDto { get; set; }
+    public LanguageScoreResultDto? LanguageScoreResultDto { get; set; }
+}
