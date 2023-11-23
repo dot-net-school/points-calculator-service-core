b102308b8f9bc5b09e692f42140effaecf7d6d52
“Mehran-Kordavani” <w.laborer@gmail.com>
2023-11-23 17:58:51 +0330

    PCS-41 <Feat: Add Customer Adapter service to connect with customer service>
    its DTOs included too

---
+++ Src/Application/Common/Interfaces/IGetCustomerAdapter.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs.CustomerService;
+using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace Application.Common.Interfaces;
+
+public interface IGetCustomerAdapter
+{
+    public Task<ReceivedCustomerInfoDto?> SendAsync(string id);
+}
---
+++ Src/Infrastructure/CustomerServiceProvider/GetCustomerAdapter.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+using Application.Common.Interfaces;
+using Application.DTOs.CustomerService;
+using Newtonsoft.Json;
+using Polly;
+using Polly.CircuitBreaker;
+
+namespace Infrastructure.CustomerServiceProvider;
+
+internal class GetCustomerAdapter : IGetCustomerAdapter
+{
+    private readonly AsyncCircuitBreakerPolicy _circuitBreakerPolicy;
+
+    //TODO remove magic string
+    // The URL of the customer service
+    private const string CustomerServiceUrl = "http://localhost:3000/getcustomer";
+
+    private readonly HttpClient _httpClient;
+
+    public GetCustomerAdapter(IHttpClientFactory httpClientFactory)
+    {
+        _circuitBreakerPolicy = Policy.Handle<Exception>()
+            .CircuitBreakerAsync(2, TimeSpan.FromMinutes(2));
+        _httpClient = httpClientFactory.CreateClient();
+    }
+
+    //TODO return operation result or DTO itself? 
+    public async Task<ReceivedCustomerInfoDto?> SendAsync(string id)
+    {
+        var response = await _circuitBreakerPolicy.ExecuteAsync(async () =>
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"{CustomerServiceUrl}/{id}", UriKind.Absolute)
+                };
+                return await _httpClient.SendAsync(request);
+            }
+        );
+        return await response.Content.ReadFromJsonAsync<ReceivedCustomerInfoDto>();
+    }
+}
